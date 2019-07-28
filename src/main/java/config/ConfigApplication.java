2fe2a22fb172ec0e7a60e230552e363cb34ce09d
jxustcmh <jxrjcmh@163.com>
2019-07-28 10:07:04 +0800

    add

---
+++ src/main/java/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jxlgcmh
+ * @date 2019-07-28 00:33
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
